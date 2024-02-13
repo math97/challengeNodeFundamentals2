9c45deba468361f2b5e786ede8aa994693d693d7
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-02-13 16:04:26 +0000

    Add server,routes and utils

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
-import http from 'http'
+import http from 'node:http'
 
-const PORT='3333'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
-const server = http.createServer(async (req,res)=>{
-    return res.end('hello world')
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
+
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
+  }
+
+  return res.writeHead(404).end()
 })
 
-server.listen(PORT)
+server.listen(3333)
